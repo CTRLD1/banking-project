1e1116d21e5867779e3082465a5ebbf6d5c74d9f
dansubaie2 <dansubaie029@gmail.com>
2025-09-22 11:44:40 +0300

    Add Customer class with only the basics (id, name, password, checking or savings or both)

---
+++ bank/customer.py
@@ -0,0 +1,25 @@
+from account import Account
+
+class Customer:
+    def __init__(self, account_id, first_name, last_name, password, has_checking = True, has_savings = True ):
+        self.account_id = account_id
+        self.first_name = first_name
+        self.last_name = last_name
+        self.password = password
+
+        # George's recommendation: "I’d just put None in any account that the user didn’t have. This way a user can have 0 without the account being ‘closed’".
+        self.checking = Account('checking') if has_checking else None
+        self.savings = Account('savings') if has_savings else None
+
+    def __str__(self):
+        return (f'{self.account_id}: {self.first_name} {self.last_name}')
+    
+
+
+
+if __name__ == '__main__':
+    # this is where the user faceing "program" goes
+    customer1= Customer(1021, 'Danah', 'Alsubaie', '1234', has_checking= True, has_savings= False)
+    print(customer1)
+    print('checking account:', customer1.checking)
+    print('savings account:', customer1.savings)
