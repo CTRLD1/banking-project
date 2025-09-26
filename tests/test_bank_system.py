e3b0e0b56fb88a52ef9838447a2ec7ae1d0b62b0
dansubaie2 <dansubaie029@gmail.com>
2025-09-26 13:54:48 +0300

    Fixed the failed withdraw unittest in test_bank_system and added one more test for transfer between users method

--- tests/test_bank_system.py
+++ tests/test_bank_system.py
@@ -34,7 +34,7 @@ class TestBankSystem(unittest.TestCase):
         self.assertEqual(new_customer.frst_name, 'Danah')
         self.assertEqual(new_customer.last_name, 'Alsubaie')
         
-        # to make sure taht the new customer was added in testing and before the tearDown deletes it
+        # to make sure that the new customer was added in testing and before the tearDown deletes it
         with open('test_bank.csv', 'r') as f:
             lines = f.readlines()
             last_line = lines[-1].strip()
@@ -56,9 +56,9 @@ class TestBankSystem(unittest.TestCase):
 
     # testing deposit (required login)
     def test_deposit_success(self):
-        # 10000 + 200 = 10200
+        # 9800 + 200 = 10000
         customer = self.bank.deposit(10002,'idh36%@#FGd', 'checking', 200) 
-        self.assertEqual(customer.checking.balance, 10200)
+        self.assertEqual(customer.checking.balance, 10000)
 
     def test_deposit_invalid(self):
         with self.assertRaises(ValueError):
@@ -70,4 +70,24 @@ class TestBankSystem(unittest.TestCase):
         customer = self.bank.withdraw(10002,'idh36%@#FGd', 'checking', 100) 
         self.assertEqual(customer.checking.balance, 9700)
 
-    
+
+    # transfer between users tests
+    def test_transfer_between_users(self):
+        # transfer from 10005 to 10006
+        self.bank.transfer_between_useres(
+            sender_id=10005, sender_pass='d^dg23g)@', sender_type='checking',
+            receiver_id=10006, receiver_type='savings', 
+            amount= 1000
+        )
+
+        sender = self.bank.login(10005, 'd^dg23g)@')
+        receiver = self.bank.login(10006, 'd1234')
+        
+        # 99000 - 1000 = 98000
+        self.assertEqual(sender.checking.balance, 98000)
+        # 1000 + 1000 = 2000
+        self.assertEqual(receiver.savings.balance, 2000)
+
+
+if __name__ == '__main__':
+    unittest.main()
